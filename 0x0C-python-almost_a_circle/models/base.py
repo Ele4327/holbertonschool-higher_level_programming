4afe3efc0cd9e3fbb2a9105fa2e324699f744cd3
ele4327 <sebasgarzons88@gmail.com>
2021-10-18 22:09:31 -0500

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 
 class Base:
+
+    """
+        Define a "Base" of all clases of the project, base model
+
+        Attributes:
+            __nb_objects (int): Number of objects(instances)
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+
+        """Constructor to initialice a new instance of base.
+
+        Args:
+            id (int): ID of each base instance
+
+        """
 
         if id is not None:
             self.id = id
